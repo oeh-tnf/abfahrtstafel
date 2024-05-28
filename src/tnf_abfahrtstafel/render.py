a45c33e0253ae2930cccb712528025489a97fa6e
jstsmthrgk <jstsmthrgk@jstsmthrgk.eu>
2024-05-28 16:33:59 +0200

    querformat

--- src/tnf_abfahrtstafel/render.py
+++ src/tnf_abfahrtstafel/render.py
@@ -3,8 +3,8 @@ from datetime import timedelta
 import math
 from PIL import Image, ImageDraw, ImageFont
 
-width = 680
-height = 960
+width = 960
+height = 680
 
 font = ImageFont.truetype(os.path.join(os.path.dirname(__file__),'Roboto-Bold.ttf'), 100)
 
@@ -16,20 +16,20 @@ def render_departures(departures):
   draw.text((15, 5), '1', font=font, fill=1)
 
   if '1' in departures:
-    offset = 150
+    offset = 200
     for dep in departures['1']:
       minutes = math.floor(max(dep / timedelta(minutes=1), 0))
       draw.text((offset, 5), f'{minutes}\'', font=font)
-      offset += 180
+      offset += 200
 
   draw.rounded_rectangle([10,110+10,10+65,110+10+100], 5, fill=0)
   draw.text((15, 110+5), '2', font=font, fill=1)
 
   if '2' in departures:
-    offset = 150
+    offset = 200
     for dep in departures['2']:
       minutes = math.floor(max(dep / timedelta(minutes=1), 0))
       draw.text((offset, 110+5), f'{minutes}\'', font=font)
-      offset += 180
+      offset += 200
 
   return image
